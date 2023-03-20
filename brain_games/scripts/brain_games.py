008bb87316f4a304a38cdbb276127535c8cdc4da
Oleja Iv <oleg1242@gmail.com>
2023-03-20 20:17:01 +0300

    Script main added

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,14 @@
+def greet(who):
+    print(f'Hello, {who}!')
+
+
+def welcome_greet():
+    print('Welcome to the Brain Games!')
+
+
+def main():
+    welcome_greet()
+
+
+if __name__ == '__main__':
+    main()
