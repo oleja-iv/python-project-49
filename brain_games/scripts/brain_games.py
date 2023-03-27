4d587aad07cdb6c593883044b5be7aee5ca16cf7
Oleja Iv <oleg1242@gmail.com>
2023-03-27 13:55:28 +0300

    Step 6. Fixed docstring

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -4,12 +4,11 @@ This is a simple command-line game.
 
 Asks person for name greets them
 """
-
 from brain_games.cli import welcome_user
 
 
 def main():
-    """File is loaded as script, greet the person."""
+    """Greet the person."""
     welcome_user()
 
 
