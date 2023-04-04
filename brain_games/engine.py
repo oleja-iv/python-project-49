b134897e9ccc868bfaf629a235f464df4c664319
Oleja Iv <oleg1242@gmail.com>
2023-04-04 22:05:55 +0300

    Functions merged from logic.py to engine.py

---
+++ brain_games/engine.py
@@ -0,0 +1,49 @@
+"""The maintenance of all games."""
+import prompt
+import brain_games.cli
+
+
+def is_correct_answer(question, correct_answer):
+    """
+    Ask a question and check answer.
+
+    Args:
+        question: what the user has to solve
+        correct_answer: correct answer
+
+    Returns:
+        True if answer is correct, false in otherwise
+    """
+    print('Question: {qstn}'.format(qstn=question))
+    answer = prompt.string('Your answer: ')
+
+    if answer == correct_answer:
+        print('Correct!')
+        return True
+
+    print(
+        "'{wrong}' is wrong answer ;(. Correct answer was '{correct}'.".format(
+            wrong=answer, correct=correct_answer,
+        ))
+    return False
+
+
+def start_the(game_name):
+    """
+    Start the game and check 4 results.
+
+    Args:
+        game_name: what game we will start
+    """
+    name = brain_games.cli.welcome_user()
+    game_name.give_a_task()
+
+    successful_rounds = 0
+    while successful_rounds < 3:
+        task, correct_answer = game_name.start_round()
+        if not is_correct_answer(task, correct_answer):
+            print("Let's try again, {who}!".format(who=name))
+            break
+        successful_rounds += 1
+    if successful_rounds == 3:
+        print('Congratulations, {who}!'.format(who=name))
