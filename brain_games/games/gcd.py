9ae6cb1c7396959d3a310df9fb36795d7d5339d9
Oleja Iv <oleg1242@gmail.com>
2023-04-05 14:05:56 +0300

    Functions import merged to engine.py. Fixed style

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -2,12 +2,11 @@
 import math
 import random
 
-from brain_games.games.logic import is_correct_answer
-
-rand = 15  # max value of start dividers in gcd game
+RANDOM_LIMIT = 15  # max value of start dividers in gcd game
 
 
 def give_a_task():
+    """Show a task."""
     print('Find the greatest common divisor of given numbers.')
 
 
@@ -18,13 +17,14 @@ def generate_the_pair():
     Returns:
          Tuple with pair
     """
-    num1 = random.randint(0, rand)
-    num2 = random.randint(0, rand)
-    num3 = random.randint(0, rand)
-    first = num1 * num3
-    second = num2 * num3
+    random_num1 = random.randint(0, RANDOM_LIMIT)
+    random_num2 = random.randint(0, RANDOM_LIMIT)
+    common_divider = random.randint(0, RANDOM_LIMIT)
 
-    return first, second
+    first_pseudo_random = random_num1 * common_divider
+    second_pseudo_random = random_num2 * common_divider
+
+    return first_pseudo_random, second_pseudo_random
 
 
 def start_round():
@@ -32,13 +32,12 @@ def start_round():
     Answer the question three times, find the greatest common divisor.
 
     Returns:
-         True if user is a winner, False in otherwise
+         pair: two numbers, where user will find the gcd
+         correct_answer: correct answer
     """
-    first, second = generate_the_pair()
+    first_random_number, second_random_number = generate_the_pair()
 
-    correct_answer = str(math.gcd(first, second))
-    pair = '{x} {y}'.format(x=first, y=second)
+    correct_answer = str(math.gcd(first_random_number, second_random_number))
+    pair = '{x} {y}'.format(x=first_random_number, y=second_random_number)
 
-    if not is_correct_answer(pair, correct_answer):
-        return False
-    return True
+    return pair, correct_answer
