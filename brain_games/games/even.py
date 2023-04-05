12473a77b99b9f501ab0455c5843bb151b047e04
Oleja Iv <oleg1242@gmail.com>
2023-04-05 13:06:24 +0300

    Functions import merged to engine.py. Fixed docstring

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -2,9 +2,7 @@
 
 import random
 
-from brain_games.games.logic import is_correct_answer
-
-rand = 30  # max value of in questions for even, progression and calc games
+RANDOM_LIMIT = 30  # max value of in questions for even, progression and calc games
 
 
 def give_a_task():
@@ -13,13 +11,12 @@ def give_a_task():
 
 def start_round():
     """
-    Answer the question three times: even number or not.
+    Generate even or not number and solution.
 
     Returns:
-         True if user is a winner, False in otherwise
+         random_number: task that should be resolved
+         correct_answer: correct answer for the task
     """
-    rand_num = random.randint(-rand, rand)
-    correct_answer = 'no' if rand_num % 2 else 'yes'
-    if not is_correct_answer(rand_num, correct_answer):
-        return False
-    return True
+    random_number = random.randint(-RANDOM_LIMIT, RANDOM_LIMIT)
+    correct_answer = 'no' if random_number % 2 else 'yes'
+    return random_number, correct_answer
